deea3f6e636c9b89e8a8491421e436c5a13fdc64
thais <tatasouzalindinha@gmail.com>
2022-01-20 20:12:07 -0300

    Commitando versão proxima da final (falta ajustar cria pedido)

--- app/cliente.py
+++ app/cliente.py
@@ -11,8 +11,14 @@ import sys
 from socket import *
 import random
 import json
+from time import sleep, time
 import util
 from mensagens import *
+
+def encerraConexao(tcp):
+  print('Conexão encerrada')
+  tcp.close()
+  exit()
 
 def main(login, senha):
 
@@ -28,7 +34,6 @@ def main(login, senha):
   print(f'Usuário: {login}')
   print(f'Senha: {senha}')
   
-
   # Conectando no servidor
   tcp.connect((HOST, PORT))
   dest = (HOST, PORT)
@@ -51,130 +56,55 @@ def main(login, senha):
 
   # Encerrando conexão caso token não tenha sido gerado
   if (token == -1):
-    print('Conexão encerrada')
-    tcp.close()
-    exit()
-
+    encerraConexao(tcp)
 
 #LOOP------------------------
-  # Solicitando lista de pedidos
-  print("\nSolicitando os pedidos que já fiz...")
-  solListaPedidos = ListaPedidosReq()
-  msg = solListaPedidos.pack(token)
-  tcp.send(msg)
-
-  # Recebe quantidade de pedidos feitos
-  msgQtdPedidos = PossuiPedidos()
-  msg = tcp.recv(msgQtdPedidos.tamanho)
-  msgQtdPedidos.unpack(msg)
-  print("Lista de pedidos recebida com sucesso!\n")
-  print(f'{dest}Você possui {msgQtdPedidos.qtdPedidos} itens pedidos!')
-
-  # Recebe pedidos
-  print('-----------------Meus pedidos----------------')
-  while True and msgQtdPedidos.qtdPedidos > 0:
-    pedidoRes = PedidoRes()
-    msg = tcp.recv(pedidoRes.tamanho)
-    pedidoRes.unpack(msg)
-    
-    print(f'Pedido: {pedidoRes.idPedido}')
-    print(f'Item: {pedidoRes.item}\tFlag: {pedidoRes.flag}')
-    print(f'Quantidade: {pedidoRes.quantidade}\tValor Unitario: {pedidoRes.valorUnitario}')
-    print()
-
-    if pedidoRes.flag == 1:
-      break
-  print('---------------------------------------------\n')
-
-  #Solicitando lista de estoque
-  print("Solicitando estoque da loja...\n")
-  solListaEstoque = EstoqueReq()
-  msg = solListaEstoque.pack(token)
-  tcp.send(msg)
-
-  # Recebendo estoque
-  print("Estoque recebido com sucesso!\n")
-  print('--------------Estoque da loja----------------')
-  estoque = []
   while True:
-    estoqueRes= EstoqueRes()
-    msg = tcp.recv(estoqueRes.tamanho)
-    estoqueRes.unpack(msg)
 
-    item = {
-      'item' : estoqueRes.item,
-      'descricao' : estoqueRes.descricao,
-      'quantidade' : estoqueRes.quantidade,
-      'valorUnitario' : estoqueRes.valorUnitario 
-    }
-    estoque.append(item)
+    # Solicitando lista de pedidos
+    print("\nSolicitando os pedidos que já fiz...")
+    solListaPedidos = ListaPedidosReq()
+    msg = solListaPedidos.pack(token)
+    tcp.send(msg)
 
-    #Imprimindo estoque
-    print(f'Item: {estoqueRes.item}\tDescrição: {estoqueRes.descricao}')
-    print(f'Quantidade: {estoqueRes.quantidade}\tValor Unitario: {estoqueRes.valorUnitario}')
-    print(f'Flag: {estoqueRes.flag}')
-    print()
+    # Recebe quantidade de pedidos feitos
+    msgQtdPedidos = PossuiPedidos()
+    msg = tcp.recv(msgQtdPedidos.tamanho)
+    msgQtdPedidos.unpack(msg)
+    print("Lista de pedidos recebida com sucesso!\n")
+    print(f'{dest}Você possui {msgQtdPedidos.qtdPedidos} itens pedidos!')
 
-    if estoqueRes.flag == 1:
-      break
-  print('---------------------------------------------\n')
-
-  # Escolhe de forma aleatória se deseja realizar pedido
-  print('Deseja criar um pedido?', end=' ')
-  escolha = random.randint(0,1)
-
-  #Enviando pro servidor a escolha 
-  criacaoPedidoReq = CriacaoPedidoReq() 
-  msg = criacaoPedidoReq.pack(escolha, token)
-  tcp.send(msg)
-
-  #Fica fazendo pedidos 
-  while (escolha == 1):
-
-    print('Sim 😀')
-
-    # Cria pedido 
-    print("Criando pedido...\n")
-    pedidos = util.criaPedidoConsumidor(estoque)
-
-    print('------------Novo pedido------------')
-    print(json.dumps(pedidos, indent=2))
-    print('-----------------------------------\n')
-    
-    # Enviando pedido pro servidor
-    for pedido in pedidos:
-      pedidoClienteRes = PedidoClienteRes()
-      flag = 0
-      if pedido == pedidos[-1]:
-        flag = 1
+    # Recebe pedidos
+    print('-----------------Meus pedidos----------------')
+    while True and msgQtdPedidos.qtdPedidos > 0:
+      pedidoRes = PedidoRes()
+      msg = tcp.recv(pedidoRes.tamanho)
+      pedidoRes.unpack(msg)
       
-      msg = pedidoClienteRes.pack(pedido['item'], pedido['qtdPedida'], pedido['valorUnitario'], flag)
-      tcp.send(msg)
+      print(f'Pedido: {pedidoRes.idPedido}')
+      print(f'Item: {pedidoRes.item}\tFlag: {pedidoRes.flag}')
+      print(f'Quantidade: {pedidoRes.quantidade}\tValor Unitario: {pedidoRes.valorUnitario}')
+      print()
 
-    # Recebe se pedido tem disponibilidade
-    disponibilidadeRes = DisponibilidadeRes()
-    msg = tcp.recv(disponibilidadeRes.tamanho)
-    disponibilidadeRes.unpack(msg)
-    token = disponibilidadeRes.token
-    print(disponibilidadeRes.mensagem)
-    
+      if pedidoRes.flag == 1:
+        break
+    print('---------------------------------------------\n')
 
-    # Conexão encerrada devido a falta de disponibilidade em estoque
-    if token == -1: # Não tem disponibilidade
-      print('Conexão encerrada')
-      tcp.close()
-      exit()
-
-    print('Pedido gerado com sucesso!')
-    print()
+    #Solicitando lista de estoque
+    print("Solicitando estoque da loja...\n")
+    solListaEstoque = EstoqueReq()
+    msg = solListaEstoque.pack(token)
+    tcp.send(msg)
 
     # Recebendo estoque
-    print('--------------Estoque atualizado-----------')
+    print("Estoque recebido com sucesso!\n")
+    print('--------------Estoque da loja----------------')
     estoque = []
     while True:
-      estoqueRes = EstoqueRes()
+      estoqueRes= EstoqueRes()
       msg = tcp.recv(estoqueRes.tamanho)
       estoqueRes.unpack(msg)
+
       item = {
         'item' : estoqueRes.item,
         'descricao' : estoqueRes.descricao,
@@ -182,7 +112,7 @@ def main(login, senha):
         'valorUnitario' : estoqueRes.valorUnitario 
       }
       estoque.append(item)
-      
+
       #Imprimindo estoque
       print(f'Item: {estoqueRes.item}\tDescrição: {estoqueRes.descricao}')
       print(f'Quantidade: {estoqueRes.quantidade}\tValor Unitario: {estoqueRes.valorUnitario}')
@@ -191,17 +121,58 @@ def main(login, senha):
 
       if estoqueRes.flag == 1:
         break
-    print('----------------------------------------\n')
-    
-    print('Deseja criar um novo pedido?', end=' ')
-    escolha = random.randint(0,1)
+    print('---------------------------------------------\n')
+
+    # Escolhe de forma aleatória se deseja realizar pedido
+    print('Deseja criar um pedido?', end=' ')
+    escolha = random.randint(1,1)
+    sleep(13)
+
+    #Enviando pro servidor a escolha 
+    criacaoPedidoReq = CriacaoPedidoReq() 
     msg = criacaoPedidoReq.pack(escolha, token)
     tcp.send(msg)
-  else:
-    print("Não") 
-    print('Conexão encerrada 😢')
-    tcp.close()
-    exit()
+
+    if (escolha == 1):
+    
+      print('Sim 😀')
+
+      # Cria pedido 
+      print("Criando pedido...\n")
+      pedidos = util.criaPedidoConsumidor(estoque)
+
+      print('------------Novo pedido------------')
+      print(json.dumps(pedidos, indent=2))
+      print('-----------------------------------\n')
+      
+      # Enviando pedido pro servidor
+      for pedido in pedidos:
+        pedidoClienteRes = PedidoClienteRes()
+        flag = 0
+        if pedido == pedidos[-1]:
+          flag = 1
+        
+        msg = pedidoClienteRes.pack(pedido['item'], pedido['qtdPedida'], pedido['valorUnitario'], flag)
+        tcp.send(msg)
+
+      # Recebe se pedido tem disponibilidade
+      disponibilidadeRes = DisponibilidadeRes()
+      msg = tcp.recv(disponibilidadeRes.tamanho)
+      disponibilidadeRes.unpack(msg)
+      token = disponibilidadeRes.token
+      print(disponibilidadeRes.mensagem)
+      
+      # Conexão encerrada devido a falta de disponibilidade em estoque
+      if token == -1: # Não tem disponibilidade
+        print('Conexão encerrada')
+        tcp.close()
+        exit()
+
+      print('Pedido gerado com sucesso!')
+      print()
+    else:
+      print("Não") 
+      encerraConexao(tcp)
   
 if __name__ == '__main__':
   if len(sys.argv) == 3:
--- app/servidor.py
+++ app/servidor.py
@@ -25,7 +25,6 @@ def encerraConexao(conexao):
   print('Conexão encerrada')
   _thread.exit()
 
-
 def autenticaClientes(login, senha):
 
   usuario = util.getUsuario(login)
@@ -64,135 +63,118 @@ def atendeCliente(conexao, cliente):
   autenticacaoRes = AutenticacaoRes()
   msg = autenticacaoRes.pack('Login efetuado com sucesso!',token)
   conexao.send(msg)
-
-  #Recebendo solicitação da lista de pedidos do usuário
-  print("\nRecebendo solicitação da lista de pedidos do usuário...")
-  solListaPedidos = ListaPedidosReq()
-  solListaPedidos.unpack(conexao.recv(solListaPedidos.tamanho))
   
-  #não tem permissão para acessar lista de pedidos
-  if solListaPedidos.token != token: 
-    encerraConexao(conexao)
-  
-  #Acessando lista de pedidos cadastrados
-  print("Solicitação atendida com sucesso!")
-  pedidos = util.getPedidosUsuario(idUsuario)
-  
-  #Enviando quantidade de pedidos que o cliente tem
-  possuiPedidos = PossuiPedidos()
-  msg = possuiPedidos.pack(len(pedidos))
-  conexao.send(msg)
-
-  #Enviando lista de pedidos para usuário
-  print("Enviando lista de pedidos do usuário...")
-  for pedido in pedidos:
-    pedidoRes = PedidoRes()
-    flag = 0
-    if pedido == pedidos[-1]:
-      flag = 1
-    conexao.send(pedidoRes.pack(pedido['id'], pedido['idItem'], pedido['quantidade'], pedido['valorUnitario'], flag))
-  print("Lista de pedidos enviada com sucesso!")
-
-  #Recebendo solicitação de estoque 
-  print("\nRecebendo solicitação de envio de estoque...")
-  solListaEstoque = EstoqueReq()
-  solListaEstoque.unpack(conexao.recv(solListaEstoque.tamanho))
-  
-  #não tem permissão para acessar estoque
-  if solListaEstoque.token != token: 
-    encerraConexao(conexao)
-
-  #Pegando estoque 
-  print("Solicitação atendida com sucesso!")
-  estoque = util.getEstoque()
-
-  #Enviando lista de estoque para usuário
-  print("Enviando estoque...")
-  for e in estoque:
-    estoqueRes = EstoqueRes()
-    flag = 0
-    if e == estoque[-1]: #se for a última linha
-      flag = 1
-    conexao.send(estoqueRes.pack(e['item'], e['descricao'], e['quantidade'], e['valorUnitario'], flag))
-  print("Estoque enviado com sucesso!")
-
-  # Recebendo se o cliente deseja realizar um pedido
-  criacaoPedidoReq = CriacaoPedidoReq()
-  criacaoPedidoReq.unpack(conexao.recv(criacaoPedidoReq.tamanho))
-  deseja = 0
-
-  while criacaoPedidoReq.flag == 1: 
-    print("\nCliente irá criar um pedido...")
-    deseja = 1
-    listaItensPedido = []
+  while True:
+    #Recebendo solicitação da lista de pedidos do usuário
+    print("\nRecebendo solicitação da lista de pedidos do usuário...")
+    solListaPedidos = ListaPedidosReq()
+    solListaPedidos.unpack(conexao.recv(solListaPedidos.tamanho))
     
-    # Recebe pedidos do cliente
-    print('\nImprimindo pedido recebido: ')
-    print('\n############################')
-    while True: # Percorre itens do pedido até o pedido acabar
-      pedidoClienteRes = PedidoClienteRes()
-      pedidoClienteRes.unpack(conexao.recv(pedidoClienteRes.tamanho))
-
-      itemPedido = {
-        'item': pedidoClienteRes.item,
-        'qtdPedida': pedidoClienteRes.quantidade
-      }
-
-      listaItensPedido.append(itemPedido) # Pedido do cliente
-      
-      #Imprimindo pedido do cliente
-      print(f'Item: {pedidoClienteRes.item}')
-      print(f'Quantidade: {pedidoClienteRes.quantidade}')
-      print(f'Valor Unitario: {pedidoClienteRes.valorUnitario}')
-      print(f'Flag: {pedidoClienteRes.flag}')
-      print('############################')
-
-      if pedidoClienteRes.flag == 1:
-        break
+    #não tem permissão para acessar lista de pedidos
+    if solListaPedidos.token != token: 
+      encerraConexao(conexao)
+  
+    #Acessando lista de pedidos cadastrados
+    print("Solicitação atendida com sucesso!")
+    pedidos = util.getPedidosUsuario(idUsuario)
     
-    #Verifica disponibilidade do pedido recebido
-    pedido, estoqueAtualizado = util.atendePedidoCliente(listaItensPedido)
+    #Enviando quantidade de pedidos que o cliente tem
+    possuiPedidos = PossuiPedidos()
+    msg = possuiPedidos.pack(len(pedidos))
+    conexao.send(msg)
 
-    #Informa cliente que pedido não tem disponibilidade
-    if not pedido: # Não tem disponibilidade de estoque
-      print('\n----- Pedido rejeitado -----')
+    #Enviando lista de pedidos para usuário
+    print("Enviando lista de pedidos do usuário...")
+    for pedido in pedidos:
+      pedidoRes = PedidoRes()
+      flag = 0
+      if pedido == pedidos[-1]:
+        flag = 1
+      conexao.send(pedidoRes.pack(pedido['id'], pedido['idItem'], pedido['quantidade'], pedido['valorUnitario'], flag))
+    print("Lista de pedidos enviada com sucesso!")
 
-      disponibilidadeRes = DisponibilidadeRes()
-      msg = disponibilidadeRes.pack('Pedido rejeitado - Não há disponibilidade de estoque', -1)
-      conexao.send(msg)
-
-      #Encerra conexão
+    #Recebendo solicitação de estoque 
+    print("\nRecebendo solicitação de envio de estoque...")
+    solListaEstoque = EstoqueReq()
+    solListaEstoque.unpack(conexao.recv(solListaEstoque.tamanho))
+  
+    #não tem permissão para acessar estoque
+    if solListaEstoque.token != token: 
       encerraConexao(conexao)
 
-    # Informa cliente que pedido tem disponibilidade
-    disponibilidadeRes = DisponibilidadeRes()
-    msg = disponibilidadeRes.pack('Pedido aceito - Há disponibilidade de estoque', token)
-    conexao.send(msg)
-    print('\nPedido tem disponibilidade em estoque')
-    print('Pedido efetuado com sucesso!')
-    print("Número do seu pedido: ",pedido['numero_pedido'])
+    #Pegando estoque 
+    print("Solicitação atendida com sucesso!")
+    estoque = util.getEstoque()
 
-    #Salvando pedido realizado no arquivo de pedidos
-    util.atualizaPedidos(pedido,idUsuario)
-
-    #Enviando estoque atualizado para cliente
-    for e in estoqueAtualizado:
+    #Enviando lista de estoque para usuário
+    print("Enviando estoque...")
+    for e in estoque:
       estoqueRes = EstoqueRes()
       flag = 0
-      if e == estoqueAtualizado[-1]: #se for a última linha
+      if e == estoque[-1]: #se for a última linha
         flag = 1
       conexao.send(estoqueRes.pack(e['item'], e['descricao'], e['quantidade'], e['valorUnitario'], flag))
-    
+    print("Estoque enviado com sucesso!")
+
+    # Recebendo se o cliente deseja realizar um pedido
+    criacaoPedidoReq = CriacaoPedidoReq()
     criacaoPedidoReq.unpack(conexao.recv(criacaoPedidoReq.tamanho))
-  else:  #Não quer criar novo pedido
-    
-    if deseja == 1:
+
+    if criacaoPedidoReq.flag == 1: 
+      print("\nCliente irá criar um pedido...")
+      listaItensPedido = []
+      
+      # Recebe pedidos do cliente
+      print('\nImprimindo pedido recebido: ')
+      print('\n############################')
+      while True: # Percorre itens do pedido até o pedido acabar
+        pedidoClienteRes = PedidoClienteRes()
+        pedidoClienteRes.unpack(conexao.recv(pedidoClienteRes.tamanho))
+
+        itemPedido = {
+          'item': pedidoClienteRes.item,
+          'qtdPedida': pedidoClienteRes.quantidade
+        }
+
+        listaItensPedido.append(itemPedido) # Pedido do cliente
+        
+        #Imprimindo pedido do cliente
+        print(f'Item: {pedidoClienteRes.item}')
+        print(f'Quantidade: {pedidoClienteRes.quantidade}')
+        print(f'Valor Unitario: {pedidoClienteRes.valorUnitario}')
+        print(f'Flag: {pedidoClienteRes.flag}')
+        print('############################')
+
+        if pedidoClienteRes.flag == 1:
+          break
+      
+      #Verifica disponibilidade do pedido recebido
+      pedido, estoqueAtualizado = util.atendePedidoCliente(listaItensPedido)
+
+      #Informa cliente que pedido não tem disponibilidade
+      if not pedido: # Não tem disponibilidade de estoque
+        print('\n----- Pedido rejeitado -----')
+
+        disponibilidadeRes = DisponibilidadeRes()
+        msg = disponibilidadeRes.pack('Pedido rejeitado - Não há disponibilidade de estoque', -1)
+        conexao.send(msg)
+
+        #Encerra conexão
+        encerraConexao(conexao)
+
+      # Informa cliente que pedido tem disponibilidade
+      disponibilidadeRes = DisponibilidadeRes()
+      msg = disponibilidadeRes.pack('Pedido aceito - Há disponibilidade de estoque', token)
+      conexao.send(msg)
+      print('\nPedido tem disponibilidade em estoque')
+      print('Pedido efetuado com sucesso!')
+      print("Número do seu pedido: ",pedido['numero_pedido'])
+
+      #Salvando pedido realizado no arquivo de pedidos
+      util.atualizaPedidos(pedido,idUsuario)
+    else:  #Não quer criar novo pedido
       print("\nCliente não quis criar mais pedidos")
-    else:
-      print("\nCliente não quis criar pedido")
-    conexao.close()
-    print('Conexão encerrada')
-    _thread.exit()
+      encerraConexao(conexao)
 
 # Configuração do socket tcp
 tcp = socket(AF_INET, SOCK_STREAM)
